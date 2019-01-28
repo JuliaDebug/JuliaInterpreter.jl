5ed5eb8cf8d79fbe0c6f5b9ce0b14ddbb0cb65fb
Tim Holy <tim.holy@gmail.com>
2019-01-28 11:42:31 -0600

    Add test for literal expressions
    Fixes #22

--- test/misc.jl
+++ test/misc.jl
@@ -20,3 +20,9 @@ execute_command(state, state.stack[1], Val{:finish}(), "finish")
       runframe(@make_stack(complicated_keyword_stuff(1, 2))[1])
 @test runframe(ASTInterpreter2.enter_call(complicated_keyword_stuff, 1, 2; x=7, y=33)) ==
       runframe(@make_stack(complicated_keyword_stuff(1, 2; x=7, y=33))[1])
+
+# Issue #22
+f22() = string(:(a+b))
+@test step_through(enter_call_expr(:($f22()))) == "a + b"
+f22() = string(QuoteNode(:a))
+@test step_through(enter_call_expr(:($f22()))) == ":a"
