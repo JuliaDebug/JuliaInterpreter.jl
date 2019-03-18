0ee85aeec20fefebb5cd26da0bf9a33385e298ca
Kristoffer Carlsson <kristoffer.carlsson@chalmers.se>
2019-03-18 19:22:07 +0100

    fix what project is used when building builtins (#174)
    * fix what project is used when building builtins
    
    * Update JuliaInterpreter.jl
    
    * Update src/JuliaInterpreter.jl
    
    Co-Authored-By: KristofferC <kristoffer.carlsson@chalmers.se>

--- src/JuliaInterpreter.jl
+++ src/JuliaInterpreter.jl
@@ -25,7 +25,9 @@ const BUILTIN_FILE = joinpath(@__DIR__, "builtins-julia$(Int(VERSION.major)).$(I
 @info "Generating builtins for this julia version..."
 gen_builtins_file = joinpath(@__DIR__, "generate_builtins.jl")
 # Run as separate command to prevent including the generate_builtins into the precompile cache
-run(`$(Base.julia_cmd()) --startup-file=no $gen_builtins_file`)
+withenv("JULIA_LOAD_PATH" => joinpath(@__DIR__, "..")) do
+    run(`$(Base.julia_cmd()) --startup-file=no $gen_builtins_file`)
+end
 include_dependency(gen_builtins_file)
 
 include("types.jl")
