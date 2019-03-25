fb6557da7b8326ddbede7b7831fd9e3f28227ed5
Kristoffer Carlsson <kcarlsson89@gmail.com>
2019-03-25 11:31:47 +0100

    add docs

--- src/types.jl
+++ src/types.jl
@@ -141,6 +141,11 @@ Fields:
 - `pc`: the program counter (integer index of the next statment to be evaluated) for this frame
 - `caller`: the parent caller of this frame, or `nothing`
 - `callee`: the frame called by this one, or `nothing`
+
+The `Base` functions `show_backtrace` and `display_error` are overloaded such that
+`show_backtrace(io::IO, frame::Frame)` and `display_error(io::IO, er, frame::Frame)`
+shows a backtrace or error, respectively, in a similar way as to how Base shows
+them.
 """
 mutable struct Frame
     framecode::FrameCode
