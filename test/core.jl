227bd4b09a3da8a6debbecf5b2b18e2bc0fdac56
Tim Holy <tim.holy@gmail.com>
2020-08-20 18:28:40 -0500

    Improve robustness of `framecode_lines` (#410)

--- test/core.jl
+++ test/core.jl
@@ -6,4 +6,21 @@ using Test
     @test !JuliaInterpreter.is_quoted_type(QuoteNode(Int32), :Int64)
     @test !JuliaInterpreter.is_quoted_type(QuoteNode(Int32(0)), :Int32)
     @test !JuliaInterpreter.is_quoted_type(Int32, :Int32)
+
+    function buildexpr()
+        items = [7, 3]
+        ex = quote
+            X = $items
+            for x in X
+                println(x)
+            end
+        end
+        return ex
+    end
+    frame = JuliaInterpreter.enter_call(buildexpr)
+    lines = JuliaInterpreter.framecode_lines(frame.framecode.src)
+    # Test that the :copyast ends up on the same line as the println
+    if isdefined(Base.IRShow, :show_ir_stmt)   # only works on Julia 1.6 and higher
+        @test any(str->occursin(":copyast", str) && occursin("println", str), lines)
+    end
 end
