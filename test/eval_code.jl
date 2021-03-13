179b9faecaf00f225d349cfdeddc74b68f9c213b
Simeon Schaub <simeondavidschaub99@gmail.com>
2021-03-13 16:46:13 +0100

    always use QuoteNode in eval_code

--- test/eval_code.jl
+++ test/eval_code.jl
@@ -96,3 +96,9 @@ JuliaInterpreter.step_expr!(fr)
 @test eval_code(fr, "output") == :sym
 eval_code(fr, "output = :foo")
 @test eval_code(fr, "output") == :foo
+
+let f() = (x = GlobalRef(Main, :doesnotexist);)
+    fr = JuliaInterpreter.enter_call(f)
+    JuliaInterpreter.step_expr!(fr)
+    @test eval_code(fr, :var"%1") == GlobalRef(Main, :doesnotexist)
+end
