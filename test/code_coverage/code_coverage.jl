20206f442496cd2cd783d16b302733010339c5e3
Simeon David Schaub <schaub@mit.edu>
2021-10-27 19:13:50 -0400

    fix coverage test on nightly
    Depends on JuliaLang/julia#42810 to pass. I did also observe some other
    test failures I couldn't really explain.

--- test/code_coverage/code_coverage.jl
+++ test/code_coverage/code_coverage.jl
@@ -8,6 +8,8 @@ let
     end
 end
 
+using DiffUtils
+
 @testset "code coverage" begin
     out = read(`$(Base.julia_cmd()) --startup=no --project=$(dirname(dirname(@__DIR__))) --code-coverage=user
                 $(joinpath(@__DIR__(), "coverage_example.jl"))`, String)
@@ -22,6 +24,9 @@ end
     if Sys.iswindows()
         cov_data = replace(cov_data, "\r\n" => "\n")
         expected = replace(cov_data, "\r\n" => "\n")
+    end
+    if cov_data != expected
+        DiffUtils.diff(cov_data, expected)
     end
     @test cov_data == expected
 end
