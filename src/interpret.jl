d2a719e80649f2a86f9ff30efd4624e155d0bed5
goerch <jhr.walter@t-online.de>
2021-12-11 14:01:41 +0100

    Fixing indentation

--- src/interpret.jl
+++ src/interpret.jl
@@ -141,7 +141,7 @@ function collect_args(frame::Frame, call_expr::Expr; isfc::Bool=false)
         if isexpr(call_expr.args[i], :call)
             args[i] = lookup_or_eval(finish_and_return!, frame, call_expr.args[i])
         else
-        	args[i] = @lookup(mod, frame, call_expr.args[i])
+            args[i] = @lookup(mod, frame, call_expr.args[i])
         end
     end
     return args
