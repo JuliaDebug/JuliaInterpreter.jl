8c7ff304676be57c841973cc3917d558b9ae40d2
Simeon David Schaub <schaub@mit.edu>
2022-08-01 14:10:47 +0200

    improve builtins consistency check
    Use DeepDiffs.jl for printing nice diffs and only print diff if
    something actually changed. Also stop hardcoding 1.9 as check for the
    build being a nightly version.

--- test/check_builtins.jl
+++ test/check_builtins.jl
@@ -1,12 +1,15 @@
-using Test
+using Test, DeepDiffs
 
-@static if VERSION >= v"1.9.0-"
+@static if !Base.GIT_VERSION_INFO.tagged_commit # only run on nightly
     @testset "Check builtin.jl consistency" begin
         builtins_path = joinpath(@__DIR__, "..", "src", "builtins.jl")
         old_builtins = read(builtins_path, String)
         include("../bin/generate_builtins.jl")
         new_builtins = read(builtins_path, String)
-        print(builtins_path, old_builtins)
-        @test old_builtins == new_builtins
+        consistent = old_builtins == new_builtins
+        if !consistent
+            println(deepdiff(old_builtins, new_builtins))
+        end
+        @test consistent
     end
 end
