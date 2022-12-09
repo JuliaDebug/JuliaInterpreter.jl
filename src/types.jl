JuliaDebug/JuliaInterpreter.jl

commit ffc2d3ca86df770057e7f60e5365dc00f941cea5

improve type stability a bit

src/types.jl
@@ -442,8 +442,9 @@ same_location(bp2::BreakpointSignature, bp::BreakpointSignature) =
     bp2.f == bp.f && bp2.sig == bp.sig && bp2.line == bp.line
 function Base.show(io::IO, bp::BreakpointSignature)
     print(io, bp.f)
-    if bp.sig !== nothing
-        print(io, '(', join("::" .* string.(bp.sig.types), ", "), ')')
+    bbsig = bp.sig
+    if bbsig !== nothing
+        print(io, '(', join("::" .* string.(bpsig.types), ", "), ')')
     end
     if bp.line !== 0
         print(io, ":", bp.line)

src/utils.jl
@@ -273,8 +273,10 @@ _linetable(list::Vector, i::Integer) = list[i]::Union{Expr,LineTypes}
 function linetable(arg, i::Integer; macro_caller::Bool=false)::Union{Expr,LineTypes}
     lt = linetable(arg)
     lineinfo = _linetable(lt, i)
-    macro_caller && while lineinfo.method === Symbol("macro expansion") && lineinfo.inlined_at != 0
-        lineinfo = _linetable(lt, lineinfo.inlined_at)
+    if macro_caller
+        while lineinfo isa Core.LineInfoNode && lineinfo.method === Symbol("macro expansion") && lineinfo.inlined_at != 0
+            lineinfo = _linetable(lt, lineinfo.inlined_at)
+        end
     end
     return lineinfo
 end

commit b1afedd54f1c072e987b72d29fca73163e709817

Fix typo in signature printing
This fixes a type/error introduced in https://github.com/JuliaDebug/JuliaInterpreter.jl/commit/ffc2d3ca86df770057e7f60e5365dc00f941cea5

src/types.jl
@@ -444,7 +444,7 @@ function Base.show(io::IO, bp::BreakpointSignature)
     print(io, bp.f)
     bbsig = bp.sig
     if bbsig !== nothing
-        print(io, '(', join("::" .* string.(bpsig.types), ", "), ')')
+        print(io, '(', join("::" .* string.(bbsig.types), ", "), ')')
     end
     if bp.line !== 0
         print(io, ":", bp.line)