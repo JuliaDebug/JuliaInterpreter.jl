b552e609b5f037e40649ed1362936865d066b594
Tim Holy <tim.holy@gmail.com>
2025-04-05 06:14:40 -0500

    Update generated functions for Julia 1.12 (#668)
    Adapts to https://github.com/JuliaLang/julia/pull/57230

--- src/construct.jl
+++ src/construct.jl
@@ -88,8 +88,12 @@ end
 
 get_source(meth::Method) = Base.uncompressed_ast(meth)
 
-function get_source(g::GeneratedFunctionStub, env, file, line::Int)
-    b = g(Base.get_world_counter(), LineNumberNode(line, file), env..., g.argnames...)
+function get_source(g::GeneratedFunctionStub, source::Method, env)
+    b = @static if VERSION < v"1.12.0-DEV.1968"   # julia #57230
+        g(Base.get_world_counter(), LineNumberNode(Int(source.line), source.file), env..., g.argnames...)
+    else
+        g(Base.get_world_counter(), source, env..., g.argnames...)
+    end
     b isa CodeInfo && return b
     return eval(b)
 end
@@ -151,7 +155,7 @@ function prepare_framecode(method::Method, @nospecialize(argtypes); enter_genera
             generator = false
         else
             if is_generated(method)
-                code = get_source(method.generator, lenv, method.file, Int(method.line))
+                code = get_source(method.generator, method, lenv)
                 generator = true
             else
                 code = get_source(method)
